25eff6e73be3697e9c24f266e419e034ffe1b057
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-02-17 01:51:51 +0300

    Create links.js

---
+++ links.js
@@ -0,0 +1,15 @@
+$.getJSON('https://yuushaexa.github.io/web/links.json', function(data) {
+   
+var regex = new RegExp(data, 'i');
+            var output = '';
+            var count = 1;
+			  $.each(data, function(key, val){
+		
+      				 output += '<a href="' + val.Link + '"'
+				  output += '>' + val.Name + '</a>';
+         
+				
+			  });
+			  output += '';
+			  $('.information').html(output);         
+  })            
