ec19b301394e65b2259ebd535605800938565b23
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-02-23 02:52:36 +0300

    Update links.js

--- links.js
+++ links.js
@@ -20,10 +20,4 @@ var regex = new RegExp(data, 'i');
 })
      }, 1000);
 
-RelatedPostsWidgetSettings = {
-    "display" : "horizontal",
-   "numberOfPosts" : 10,
-    "titleImportance" : 100,
-    "excerptLength" : 0,
-    "htmlTemplate": "<div class="relatedPostBox"><img src="$imageURL$" style="float: left;max-width: 100%;margin-bottom: 5px;border-top-right-radius: 12px;border-top-left-radius: 12px;"><div style="text-align: center;line-height: 20px;transition: .3s;font-size: 14px;">$title$</div><div style="text-align:left;font-size:13;color:#3d3d3d;margin:5px 0;font-family:Inherit;"><labelsLoop><span style="background: rgba(152,164,176,0.2);padding: 3px 6px 3px 6px;border-radius: 12px;font-size: 12px;color: #585555;margin: 3px;float: left;">#$label$</span></labelsLoop></div></div>;"
-}
+<script src="https://yuushaexa.github.io/web/dev/relatedPosts.js"</script>
