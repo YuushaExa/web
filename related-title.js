7fa26ddc934ec37d6993060e16fe36417b9f7c73
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2023-02-23 03:03:12 +0300

    Update related-title.js

--- related-title.js
+++ related-title.js
@@ -1,8 +1,10 @@
-<script src="https://yuushaexa.github.io/web/dev/relatedPosts.js"</script>
+   &lt;script&gt;
 RelatedPostsWidgetSettings = {
-    "display" : "horizontal",
-   "numberOfPosts" : 10,
-    "titleImportance" : 100,
-    "excerptLength" : 0,
-    "htmlTemplate": "<div class="relatedPostBox"><img src="$imageURL$" style="float: left;max-width: 100%;margin-bottom: 5px;border-top-right-radius: 12px;border-top-left-radius: 12px;"><div style="text-align: center;line-height: 20px;transition: .3s;font-size: 14px;">$title$</div><div style="text-align:left;font-size:13;color:#3d3d3d;margin:5px 0;font-family:Inherit;"><labelsLoop><span style="background: rgba(152,164,176,0.2);padding: 3px 6px 3px 6px;border-radius: 12px;font-size: 12px;color: #585555;margin: 3px;float: left;">#$label$</span></labelsLoop></div></div>;"
+    &#39;display&#39; : &#39;horizontal&#39;,
+    &#39;numberOfPosts&#39; : 10,
+    &#39;titleImportance&#39; : 100,
+    &#39;excerptLength&#39; : 0,
+    &#39;htmlTemplate&#39; : &#39;&lt;div class=&quot;relatedPostBox&quot;&gt;&lt;img src=&quot;$imageURL$&quot; style=&quot;float: left;max-width: 100%;margin-bottom: 5px;border-top-right-radius: 12px;border-top-left-radius: 12px;&quot;&gt;&lt;div style=&quot;text-align: center;line-height: 20px;transition: .3s;font-size: 14px;&quot;&gt;$title$&lt;/div&gt;&lt;div style=&quot;text-align:left;font-size:13;color:#3d3d3d;margin:5px 0;font-family:Inherit;&quot;&gt;&lt;labelsLoop&gt;&lt;span style=&quot;background: rgba(152,164,176,0.2);padding: 3px 6px 3px 6px;border-radius: 12px;font-size: 12px;color: #585555;margin: 3px;float: left;&quot;&gt;#$label$&lt;/span&gt;&lt;/labelsLoop&gt;&lt;/div&gt;&lt;/div&gt;&#39;
 }
+&lt;/script&gt;
+&lt;script src=&quot;https://yuushaexa.github.io/web/dev/relatedPosts.js&quot;&gt;&lt;/script&gt;
